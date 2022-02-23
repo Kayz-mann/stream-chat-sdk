e2250bfacb36f857a7a081b3efa874c068201f5d
Kayz-mann <kayodefredrickbalogun@gmail.com>
2022-02-23 20:14:53 +0100

    fix: app.tsx file

--- App.tsx
+++ App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import 'react-native-gesture-handler';
-
 
 import LoginScreen from './src/Screens/Login';
-// import ChatScreen from './src/Screens/ChatScreen';
-import { ChatRoom } from './src/Screens/ChatRoom';
+import ChatScreen from './src/Screens/ChatScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -15,14 +12,9 @@ const Stack = createNativeStackNavigator();
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Chat'>
-        {/* <Stack.Screen name='Login' component={LoginScreen} /> */}
-        {/* <Stack.Screen name='Chat' component={ChatScreen} /> */}
-        <Stack.Screen
-          name="ChatRoom"
-          component={ChatRoom}
-          // options={({route}) => ({title: route.params})}
-        />
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Chat' component={ChatScreen} />
        </Stack.Navigator>
     </NavigationContainer>
   );
